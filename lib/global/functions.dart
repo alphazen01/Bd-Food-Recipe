d49f0d981e7cb5a5c789617be38386a228058379
alphazen01 <parvejmahmud555@gmail.com>
2022-01-22 12:10:11 +0600

    lauchUrl

--- lib/global/functions.dart
+++ lib/global/functions.dart
@@ -1,7 +1,26 @@
- import 'package:flutter/material.dart';
+
+
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 SliverGridDelegateWithFixedCrossAxisCount customGridDeleget() {
     return SliverGridDelegateWithFixedCrossAxisCount(
     crossAxisCount:2,
+    crossAxisSpacing: 10,
+    mainAxisSpacing: 10,
+    childAspectRatio: 0.8,
      );
   }
+//   int countIngredients(List ingredients) {
+//   return ingredients.length;
+// }
+
+// void launchURL(url) async {
+//   if (!await launch(url)) throw 'Could not launch $url';
+// }
+int countIngreients(List ingredients){
+return ingredients.length;
+}
+void launchURL(url)async{
+  if (!await launch(url)) throw 'could not launch $url';
+}
